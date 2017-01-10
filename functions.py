cbf3c4b61952d974d7b07547da1b267d5961f66e
luisguiserrano <luisgui.serrano@gmail.com>
2017-01-09 20:15:17 -0800

    Create functions.py
    These are the main functions of the agent.

---
+++ functions.py
@@ -0,0 +1,76 @@
+def grid_values(grid):
+    #print('grid_values')
+    "Convert grid into a dict of {square: char} with '0' or '.' for empties."
+    chars = [c for c in grid if c in digits or c in '0.']
+    assert len(chars) == 81
+    return dict(zip(squares, chars))
+
+def display(values):
+    #print('display')
+    "Display these values as a 2-D grid."
+    width = 1+max(len(values[s]) for s in squares)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print ''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols)
+        if r in 'CF': print line
+    print
+
+def only_choice(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def single_possibility(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+def reduce_puzzle(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        display(values)
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Use the Single Possibility Strategy
+        values = single_possibility(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+def solve(grid): return search(grid)
+
+def search(values):
+    "Using depth-first search and propagation, try all possible values."
+    values = reduce_puzzle(values)
+    if values is False:
+        print "Failing!!"
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in squares): 
+        print "Done!!"
+        return values ## Solved!
+    ## Chose the unfilled square s with the fewest possibilities
+    print "Choosing a box"
+    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    print "making choice", n, s
+    #print display(values)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
