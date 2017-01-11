34f4eb578074dc283a759615f8c67c6938f7c06a
Luis Serrano <luisgui.serrano@gmail.com>
2017-01-11 02:49:12 -0800

    Fixing functions.py

--- functions.py
+++ functions.py
@@ -1,14 +1,4 @@
 digits   = '123456789'
-rows     = 'ABCDEFGHI'
-cols     = digits
-squares  = cross(rows, cols)
-unitlist = ([cross(rows, c) for c in cols] +
-            [cross(r, cols) for r in rows] +
-            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
-units = dict((s, [u for u in unitlist if s in u]) 
-             for s in squares)
-peers = dict((s, set(sum(units[s],[]))-set([s]))
-             for s in squares)
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
