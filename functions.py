ff12dd4196aa5f2f614b32e71b745a5d1ec331cc
Luis Serrano <luisgui.serrano@gmail.com>
2017-01-13 11:29:14 -0800

    Fixing functions.

--- functions.py
+++ functions.py
@@ -15,8 +15,6 @@ units = dict((s, [u for u in unitlist if s in u])
 peers = dict((s, set(sum(units[s],[]))-set([s]))
              for s in squares)
 
-def print_luis():
-	print("Luis")
 
 def grid_values(grid):
     "Convert grid into a dict of {square: char} with '.' for empties."
@@ -59,13 +57,12 @@ def reduce_puzzle(values):
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
-        #display(values)
         # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
         # Use the Only Choice Strategy
         values = only_choice(values)
         # Use the Single Possibility Strategy
-        values = single_possibility(values)
+        new_values = single_possibility(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
@@ -79,7 +76,7 @@ def solve(grid): return search(grid)
 
 def search(values):
     "Using depth-first search and propagation, try all possible values."
-    values = reduce_puzzle(values)
+    values = reduce_puzzle(values.copy())
     if values is False:
         #print "Failing!!"
         return False ## Failed earlier
