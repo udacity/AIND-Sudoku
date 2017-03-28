fdcdc6db52cdee460c5856415abc650d6db9310b
scbrubaker02 <scbrubaker02@gmail.com>
2017-03-28 10:35:00 -0400

    Added README submission

--- .udacity-pa/projects.py
+++ .udacity-pa/projects.py
@@ -1,12 +1,21 @@
 import argparse
+import shutil
+import os
 from udacity_pa import udacity
 
 nanodegree = 'nd889'
 projects = ['sudoku']
 
 def submit(args):
-  filenames = ['solution.py']
+  filenames = ['solution.py', 'README']
 
-  udacity.submit(nanodegree, projects[0], filenames, 
-                 environment = args.environment,
-                 jwt_path = args.jwt_path)
+  if os.path.exists('README'):
+    raise RuntimeError("Unable to copy SubmissionReadme to README because README exists already!")
+
+  shutil.copy('SubmissionReadme.md', 'README')
+  try:
+    udacity.submit(nanodegree, projects[0], filenames, 
+                   environment = args.environment,
+                   jwt_path = args.jwt_path)
+  finally:
+    os.unlink('README')
