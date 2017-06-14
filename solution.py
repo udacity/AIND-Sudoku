2caf225ad112766982d55ec34d359ad6c19f88c0
liushuwen <liushuwen0221@gmail.com>
2017-06-14 15:42:31 +0800

    add diag code

--- solution.py
+++ solution.py
@@ -37,7 +37,6 @@ def naked_twins(values):
                     for d in values[p]:
                         if d in values[box]:
                             assign_value(values, p, values[p].replace(d, ''))
-
     return values
 
 def cross(A, B):
@@ -164,13 +163,12 @@ def solve(grid):
     row_units = [cross(r, cols) for r in rows]
     column_units = [cross(rows, c) for c in cols]
     square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-    unitlist = row_units + column_units + square_units
+    diag_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'],
+                  ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+    unitlist = row_units + column_units + square_units + diag_units
     units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
     peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
     values = grid_values(grid)
-
-    eliminate(values)
-    only_choice(values)
 
     return search(values)
 
